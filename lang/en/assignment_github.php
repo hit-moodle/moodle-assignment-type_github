d0f6d588bb1161d3d274943b1e75465a374ddae2
Sun Zhigang <sunner@gmail.com>
2012-03-12 10:16:10 +0800

    Sort strings

--- lang/en/assignment_github.php
+++ lang/en/assignment_github.php
@@ -1,26 +1,21 @@
 <?php
 
-$string['typegithub'] = 'Github Project';
-$string['unknowntype'] = 'Unknown type parameter';
-$string['githubreposetting'] = 'Git Repo Setting';
-$string['repository'] = 'Repository';
-
-$string['editusername'] = 'Edit Username or organization';
-$string['editusername_help'] = 'Username or organization of the repository.';
+$string['choosegroup'] = 'Please select a group first.';
 $string['editrepository'] = 'Edit repository';
 $string['editrepository_help'] = 'The repository\'s url. Such as git://github.com/username/repository';
-
-$string['repositoryname'] = 'Repository Name';
-
-$string['repohasnotset'] = 'This user\'s or group\'s repo has not yet set.';
-$string['choosegroup'] = 'Please select a group first.';
-
+$string['editusername'] = 'Edit Username or organization';
+$string['editusername_help'] = 'Username or organization of the repository.';
+$string['githubreposetting'] = 'Git Repo Setting';
 $string['member'] = 'Member ';
 $string['member_help'] = 'Email which the member use to commit code on this repository';
 $string['memberlist'] = 'Member List';
-
-$string['unrecognizedurl'] = 'Unrecognized Url. Please check the url.';
-$string['unknownserver'] = 'Unknown Server. Please check the url.';
-$string['serviceerror'] = 'Service Error. Please check the url. If this error still happens, please contact the administrator.';
+$string['repohasnotset'] = 'This user\'s or group\'s repo has not yet set.';
 $string['reponotfind'] = 'Respository not found on the server. Please check the url.';
+$string['repository'] = 'Repository';
+$string['repositoryname'] = 'Repository Name';
+$string['serviceerror'] = 'Service Error. Please check the url. If this error still happens, please contact the administrator.';
+$string['typegithub'] = 'Github Project';
+$string['unknownserver'] = 'Unknown Server. Please check the url.';
+$string['unknowntype'] = 'Unknown type parameter';
+$string['unrecognizedurl'] = 'Unrecognized Url. Please check the url.';
 ?>
--- lang/zh_cn/assignment_github.php
+++ lang/zh_cn/assignment_github.php
@@ -1,26 +1,21 @@
 <?
 
-$string['typegithub'] = 'Github项目';
-$string['unknowntype'] = '未知的类型参数';
-$string['githubreposetting'] = 'Git容器设置';
-$string['repository'] = '容器';
-
-$string['editusername'] = '编辑Username或organization';
-$string['editusername_help'] = '容器所有者的Username或organization。';
+$string['choosegroup'] = '请先选择一个分组';
 $string['editrepository'] = '编辑容器';
 $string['editrepository_help'] = '容器的链接 例如：git://github.com/username/repository';
-
-$string['repositoryname'] = '容器名称';
-
-$string['repohasnotset'] = '此用户或分组的容器尚未设置';
-$string['choosegroup'] = '请先选择一个分组';
-
+$string['editusername'] = '编辑Username或organization';
+$string['editusername_help'] = '容器所有者的Username或organization。';
+$string['githubreposetting'] = 'Git容器设置';
 $string['member'] = '成员 ';
 $string['member_help'] = '这个小组成员在此Git容器上提交代码所用的邮箱';
 $string['memberlist'] = '成员列表';
-
-$string['unrecognizedurl'] = '无法识别的链接。请检查链接是否输入正确。';
-$string['unknownserver'] = '无法识别的托管服务器。请检查链接是否输入正确。';
-$string['serviceerror'] = '服务出错。请检查链接是否输入正确。如果仍旧发生此错误，请联系站点管理员。';
+$string['repohasnotset'] = '此用户或分组的容器尚未设置';
 $string['reponotfind'] = '未在托管服务器上找到指定的容器。请检查链接是否输入正确。';
+$string['repository'] = '容器';
+$string['repositoryname'] = '容器名称';
+$string['serviceerror'] = '服务出错。请检查链接是否输入正确。如果仍旧发生此错误，请联系站点管理员。';
+$string['typegithub'] = 'Github项目';
+$string['unknownserver'] = '无法识别的托管服务器。请检查链接是否输入正确。';
+$string['unknowntype'] = '未知的类型参数';
+$string['unrecognizedurl'] = '无法识别的链接。请检查链接是否输入正确。';
 ?>
